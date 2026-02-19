1ca26d44f597b35578a30383f1d241e824b5e3d6
rakshithapaarol <151525196+rakshithapaarol@users.noreply.github.com>
2026-02-19 11:53:25 +0530

    Update middleware.ts

--- smart-rt-bookmark-main/middleware.ts
+++ smart-rt-bookmark-main/middleware.ts
@@ -6,8 +6,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  
+  runtime: 'nodejs'
     matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    //...(?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)
   ],
 };
