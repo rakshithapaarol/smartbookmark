e027f8c023e9860afabafaf2141ecab246032117
rakshithapaarol <151525196+rakshithapaarol@users.noreply.github.com>
2026-02-19 11:49:21 +0530

    Update middleware.ts

--- smart-rt-bookmark-main/middleware.ts
+++ smart-rt-bookmark-main/middleware.ts
@@ -6,7 +6,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
+  
+    matcher: [
     "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
